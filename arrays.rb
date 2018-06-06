3e20147ee9dc34cb4267c657b7996d193d6bb8e3
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-06 20:20:52 +0000

    automatically backed up by learn

---
+++ arrays.rb
@@ -0,0 +1,10 @@
+student_names= ["Alyssa", "Ashlee"]
+#puts student_names
+#puts student_names
+student_names.push("bailey, maya")
+student_names.push("Victora")
+student_names.push("meh")
+student_names.push("Bo burnum")
+
+puts student_names.size
+puts student_names.length 
